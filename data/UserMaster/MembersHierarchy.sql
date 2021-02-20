853775b7803f64dbebc0e9025cf43297431163c7
Sagar <psagarj111@gmail.com>
2021-02-20 19:09:17 +0530

    MembersHierarchy api add

--- data/UserMaster/MembersHierarchy.sql
+++ data/UserMaster/MembersHierarchy.sql
@@ -1,7 +1,9 @@
 select 
-	  CAST(DENSE_RANK() OVER ( ORDER BY a.perentId) AS int) pid
-	  ,a.Id as id
+	--  CAST(DENSE_RANK() OVER ( ORDER BY a.perentId) AS int) pid
+	(CASE WHEN CHARINDEX('.', a.perentId, 1)=0 THEN a.perentId ELSE RIGHT(a.perentId, CHARINDEX('.', REVERSE(a.perentId)) - 1) END) as pid
+      ,a.Id as id
       ,a.RefId
+      ,a.perentId
       ,a.Name as name
       ,a.City as title
       ,CONVERT(varchar,a.DOB,105) as Dob
