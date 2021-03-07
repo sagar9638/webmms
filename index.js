d3393a530398998f281ab161283e88975e16ff59
Sagar <psagarj111@gmail.com>
2021-03-08 00:09:29 +0530

    Set Auto Designation Assign Code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 app.use('/api',UserMasterRoutes.routes);
 app.use('/api',MenuMasterRoutes.routes);
