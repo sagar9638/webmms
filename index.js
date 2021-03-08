624768f1e86d171593c3dffe34909d36238d1c0c
Sagar <psagarj111@gmail.com>
2021-03-08 23:24:36 +0530

    Set Auto Designation Assign Code

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ const DesignationDetailRoutes = require('./routes/DesignationDetailRoutes');
 const app = express();
 
 //app.use(cors());
-app.use(cors(
-    {origin:'https://mmsnodejsapi.herokuapp.com/api/'}
-));
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
+app.use(cors());
 app.use(bodyParser.json());
 
 
