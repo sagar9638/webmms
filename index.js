2a1943bd507dc8d92bdc7b9d4bbf0ceb5b93ffbe
Sagar <psagarj111@gmail.com>
2021-03-09 00:49:47 +0530

    Set Auto Designation Assign Code

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
  app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200/");
     res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
     next(); // Important
