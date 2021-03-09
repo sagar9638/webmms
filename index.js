376630a1cf63baebba754c79ed388c9794d1c9bd
Sagar <psagarj111@gmail.com>
2021-03-09 08:08:12 +0530

    Set Auto Designation Assign Code

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const DesignationDetailRoutes = require('./routes/DesignationDetailRoutes');
 const app = express();
 
 //app.use(cors());
-// app.use(cors());
+ app.use(cors());
  app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200/");
-    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
-    next(); // Important
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:4200/");
+//     res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+//     next(); // Important
+// })
 
 app.use('/api',UserMasterRoutes.routes);
 app.use('/api',MenuMasterRoutes.routes);
