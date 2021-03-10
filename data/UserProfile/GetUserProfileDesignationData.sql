ff2b2ba851dcf094f4fbec1c9801058848ac9a65
Sagar <psagarj111@gmail.com>
2021-03-11 00:57:38 +0530

    ValidLogin api changes

--- data/UserProfile/GetUserProfileDesignationData.sql
+++ data/UserProfile/GetUserProfileDesignationData.sql
@@ -11,17 +11,17 @@ SET @Query = '
 						,PreDId
 						,PreAchiveCount
 				  FROM (
-						SELECT  ISNULL(LAG(t.DName) OVER (ORDER BY t.Ord desc),'') NextDName
+						SELECT  ISNULL(LAG(t.DName) OVER (ORDER BY t.Ord desc),'''') NextDName
 							   ,ISNULL(LAG(t.DId) OVER (ORDER BY t.Ord desc),-1) NextDId
 							   ,ISNULL(LAG(t.DAchiveCount) OVER (ORDER BY t.Ord desc),-1) NextAchiveCount
 							   ,t.DName CurrDName
 							   ,t.DId CurrDId
 							   ,t.DAchiveCount CurrAchiveCount
-							   ,ISNULL(LEAD(t.DName) OVER (ORDER BY t.Ord desc),'') PreDName 
+							   ,ISNULL(LEAD(t.DName) OVER (ORDER BY t.Ord desc),'''') PreDName 
 							   ,ISNULL(LEAD(t.DId) OVER (ORDER BY t.Ord desc),-1) PreDId
 							   ,ISNULL(LEAD(t.DAchiveCount) OVER (ORDER BY t.Ord desc),-1) PreAchiveCount
 						FROM DesignationMaster t
 					   ) AS Designation
-				 WHERE 1 = 1 '+ @p_Condition;  --CurrDId = 2
+				 WHERE 1 = 1 '+ @p_Condition;  
 
 EXEC sp_executesql @Query 
